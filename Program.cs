73ff38ff97e6cd61b1f4f523207072093de26f36
Başar Çelebi <basarcelebi02@gmail.com>
2022-01-23 19:36:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("İsminizi Giriniz: ");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınızı Giriniz: ");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + surname);
+
