55e158adf82dd6fc7da7152b7c743466c3a035d2
Başar Çelebi <basarcelebi02@gmail.com>
2022-01-24 00:17:40 +0300

    variable

---
+++ degisken/degisken1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.WriteLine(" ");
+
+byte b = 5;
+sbyte c = 5; //1 byte
+
+short s = 5; //2 byte
+ushort us = 5; //0 - 365
+
+Int16 i16 = 5; //2 byte
+int i = 5; //4 byte
+Int32 i32 = 5; //4 byte
+Int64 i64 = 5; //8 byte
+
+uint ui = 4; //4 byte
+long l = 4; //8 byte
+ulong ul = 4; //8 byte
+ //Reel Sayılar
+float f = 5; //4 byte
+double d = 5; //8byte
+decimal de = 5; //16byte
+
+DateTime dt = DateTime.Now("dd.MM.yyyy");
+
+Console.WriteLine(dt);
