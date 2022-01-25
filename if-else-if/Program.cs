bb777621cd0b3ec0fc968145f703bf9009879041
Başar Çelebi <basarcelebi02@gmail.com>
2022-01-26 02:25:55 +0300

    if-else

---
+++ if-else-if/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time = DateTime.Now.Hour;
+if(time >= 6 && time < 11) 
+Console.WriteLine("Günaydın"); 
+else if(time <= 18)
+Console.WriteLine("İyi Günler");
+else
+Console.WriteLine("İyi Geceler");
+// ? = if  : = else
+string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+Console.WriteLine(sonuc);
